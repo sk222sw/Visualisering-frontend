163cc661fc275aabb50798c38af664085a8c3fb2
Mattias <mw222rs@student.lnu.se>
2016-02-10 21:01:21 +0100

    fixed hashHistory

--- src/store/initial-state.js
+++ src/store/initial-state.js
@@ -1,13 +1,13 @@
 export default {
-  sphere: {
-    data: [
-    {lat: 56.878655, lng: 14.819965, time: 300},
-    {lat: 54.242091, lng: 14.727779, time: 1020},
-    {lat: 39.399872, lng: -8.224454, time: 824},
-    {lat: 0, lng: 180, time: 404},
-    {lat: 0, lng: 90, time: 5083},
-    {lat: 0, lng: -90, time: 1204},
-    {lat: 0, lng: 0, time: 10}
-    ]
-  }
+    sphere: {
+        data: [
+        {lat: 56.878655, lng: 14.819965, time: 300},
+        {lat: 54.242091, lng: 14.727779, time: 1020},
+        {lat: 39.399872, lng: -8.224454, time: 824},
+        {lat: 0, lng: 180, time: 404},
+        {lat: 0, lng: 90, time: 5083},
+        {lat: 0, lng: -90, time: 1204},
+        {lat: 0, lng: 0, time: 10}
+        ]
+    }
 };
