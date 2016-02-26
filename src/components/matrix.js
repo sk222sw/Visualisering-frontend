0ece890c09cab1496cf88ae7cbfb0ef008e5622e
Sonny <sk222sw@student.lnu.se>
2016-02-26 23:07:09 +0100

    changed a function to js2015 syntax

--- src/components/matrix.js
+++ src/components/matrix.js
@@ -9,7 +9,7 @@ export default class GitHubSourceCode extends React.Component {
             <h2>{data.project}</h2>
             <h3>{data.githubUser}</h3>
             <h4>{data.fileName}</h4>
-            <ul>{data.contributors.map(function(contributor) {
+            <ul>{data.contributors.map(contributor => {
               return <li key={contributor.id}>{contributor}</li>;
             })}</ul>
           </div>
