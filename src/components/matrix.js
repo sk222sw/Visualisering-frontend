025b22112bd7448361e7bb3d3a10c5145cc94f3c
Sonny <sk222sw@student.lnu.se>
2016-02-27 19:47:09 +0100

    font changes

--- src/components/matrix.js
+++ src/components/matrix.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 // static texts
-const projectText = "visualizing source code from github repo: ";
+const projectText = "visualizing source code from github repo:";
 const fileNameText = "filename: ";
 const repoOwner = "owner: ";
 const contributorTitle = "contributors: ";
@@ -16,32 +16,32 @@ export default class GitHubSourceCode extends React.Component {
           </div>
           <div className="matrix-info-section">
             <div className="project-info-section">
-              <div className="project-info project-text">
+              <div className="project-info info-section-text">
                 {projectText}
               </div>
-              <h2 className="project-info project-name">{data.project}</h2>
+              <h2 className="project-info project-name info-section-name">{data.project}</h2>
             </div>
             <div className="filename-section">
-              <div className="filename-text">
+              <div className="filename-text info-section-text">
                 {fileNameText}
               </div>
-              <h4 className="filename-name">
+              <h4 className="filename-name info-section-name">
                 {data.fileName}
               </h4>
             </div>
             <div className="github-user-section">
-              <div className="github-user-text">
+              <div className="github-user-text info-section-text">
                 {repoOwner}
               </div>
-              <h3 className="github-user">{data.githubUser}</h3>
+              <h3 className="github-user info-section-name">{data.githubUser}</h3>
             </div>
             <div className="contributors-section">
-              <div className="contributors-text">
+              <div className="contributors-text info-section-text">
                 {contributorTitle}
               </div>
               <ul className="contributors-list">
                 {data.contributors.map(contributor => {
-                  return <li className="contributor" key={contributor.id}>{contributor}, </li>;
+                  return <li className="contributor info-section-name" key={contributor.id}>{contributor}, </li>;
                 })}
               </ul>
             </div>
