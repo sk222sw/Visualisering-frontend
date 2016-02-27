b5d561944eea51ada1022cb1f2f028cb53c71d81
Sonny <sk222sw@student.lnu.se>
2016-02-27 14:48:52 +0100

    early css prototype

--- src/components/matrix.js
+++ src/components/matrix.js
@@ -8,13 +8,37 @@ export default class GitHubSourceCode extends React.Component {
           <div className="matrix-code-section">
             <code>{data.sourceCode}</code>;
           </div>
-          <div className="matrix-information-section">
-            <h2>{data.project}</h2>
-            <h3>{data.githubUser}</h3>
-            <h4>{data.fileName}</h4>
-            <ul>{data.contributors.map(contributor => {
-              return <li key={contributor.id}>{contributor}</li>;
-            })}</ul>
+          <div className="matrix-info-section">
+            <div className="project-info-section">
+              <div className="project-info project-text">
+                visualizing source code from github repo:
+              </div>
+              <h2 className="project-info project-name">{data.project}</h2>
+            </div>
+            <div className="filename-section">
+              <div className="filename-text">
+                filename:
+              </div>
+              <h4 className="filename-name">
+                {data.fileName}
+              </h4>
+            </div>
+            <div className="github-user-section">
+              <div className="github-user-text">
+                owner:
+              </div>
+              <h3 className="github-user">{data.githubUser}</h3>
+            </div>
+            <div className="contributors-section">
+              <div className="contributors-text">
+                contributors:
+              </div>
+              <ul className="contributors-list">
+                {data.contributors.map(contributor => {
+                  return <li className="contributor" key={contributor.id}>{contributor}, </li>;
+                })}
+              </ul>
+            </div>
           </div>
         </div>
       );
