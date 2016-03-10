71328ece6276c890d5537fb53d27f95c54034cb1
Sonny Kjellberg <sk222sw@student.lnu.se>
2016-03-10 09:06:03 +0000

    changed data names

--- src/store/initial-state.js
+++ src/store/initial-state.js
@@ -2,27 +2,11 @@
 
 export default {
   components: {
-<<<<<<< 7f4eca5bc0b0af601d0cff3f9299173c2009abc7
     commits: [],
     positions: [],
     codeCrawl: {
       text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi officia adipisci numquam eos sit. Autem vitae accusamus corporis laudantium ipsa ad impedit, quis possimus omnis rem excepturi illo sunt reprehenderit!"
     }    
-=======
-    commits: [{
-      repo: "TRAFFICREPORTS_js223kz",
-      owner: "js223kz ",
-      timestamp: "2016-03-08T16:46:28Z",
-      message: "Caching sr-api response",
-      committer: "js223kz",
-      filename: "fil.js",
-      code: "if(hej);"
-    }],
-    positions: [],
-    codeCrawl: {
-      text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi officia adipisci numquam eos sit. Autem vitae accusamus corporis laudantium ipsa ad impedit, quis possimus omnis rem excepturi illo sunt reprehenderit!"
-    }
->>>>>>> changed data names
   },
   settings: {
     time: null
