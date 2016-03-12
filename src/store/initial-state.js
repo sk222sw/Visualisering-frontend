8e767cf343e23196d2161d09bea48bf977bc517c
Sonny Kjellberg <sk222sw@student.lnu.se>
2016-03-12 10:58:44 +0000

    changed commit/position initial state to null and deleted codeCrawl from initial state

--- src/store/initial-state.js
+++ src/store/initial-state.js
@@ -2,11 +2,8 @@
 
 export default {
   components: {
-    commits: [],
-    positions: [],
-    codeCrawl: {
-      text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi officia adipisci numquam eos sit. Autem vitae accusamus corporis laudantium ipsa ad impedit, quis possimus omnis rem excepturi illo sunt reprehenderit!"
-    }    
+    commits: null,
+    positions: null
   },
   settings: {
     time: null
